189b317136184aedec6c2f915aeb959702d03c16
brian <balsamo@uchicago.edu>
2017-02-20 09:32:07 -0600

    Added support for configuration through an environmental variable.

--- premiser/__init__.py
+++ premiser/__init__.py
@@ -4,5 +4,6 @@ from .blueprint import BLUEPRINT
 app = Flask(__name__)
 
 # app.config['tempdir'] = "/tmp"
+app.config.from_envvar('PREMISER_SETTINGS', silent=True)
 
 app.register_blueprint(BLUEPRINT)
