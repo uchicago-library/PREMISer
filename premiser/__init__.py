9ffa4da19d96d07a24a8a8fc59bb74a841576d54
brian <balsamo@uchicago.edu>
2017-02-23 14:09:33 -0600

    Bringing config envvar name in line with other microservices.

--- premiser/__init__.py
+++ premiser/__init__.py
@@ -4,6 +4,6 @@ from .blueprint import BLUEPRINT
 app = Flask(__name__)
 
 # app.config['tempdir'] = "/tmp"
-app.config.from_envvar('PREMISER_SETTINGS', silent=True)
+app.config.from_envvar('PREMISER_CONFIG', silent=True)
 
 app.register_blueprint(BLUEPRINT)
